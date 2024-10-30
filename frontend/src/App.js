40b578b86d737752249c9f77168b9104e1838c77
ANUJ NETHWEWALA <anujnethwewala@ANUJs-MacBook-Pro-2.local>
2024-10-30 23:46:09 +0530

    Added header and sidedrawer for navigation

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,17 +1,26 @@
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
 import Users from "./users/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
+import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 function App() {
   return (
     <>
       <Router>
-        <Routes>
-          <Route path="/" exact element={<Users />}></Route>
-          <Route path="/places/new" exact element={<NewPlace />}></Route>
-          <Route path="*" element={<Navigate to="/" />}></Route>
-        </Routes>
+        <MainNavigation />
+        <main>
+          <Routes>
+            <Route path="/" exact element={<Users />}></Route>
+            <Route path="/places/new" exact element={<NewPlace />}></Route>
+            <Route path="*" element={<Navigate to="/" />}></Route>
+          </Routes>
+        </main>
       </Router>
     </>
   );
