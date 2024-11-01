7f5ec1867b3e57af897153f08de17c8f4d8bfdbc
ANUJ NETHWEWALA <anujnethwewala@ANUJs-MacBook-Pro-2.local>
2024-11-01 17:46:41 +0530

    Add New Place form

--- frontend/src/places/components/PlaceItem.js
+++ frontend/src/places/components/PlaceItem.js
@@ -1,29 +1,63 @@
-import React from "react";
+import React, { useState } from "react";
 
 import Card from "../../shared/components/UIElements/Card";
 import Button from "../../shared/components/FormElements/Button";
+import Modal from "../../shared/components/UIElements/Modal";
 
 import "./PlaceItem.css";
 
 const PlaceItem = (props) => {
+  const [showMap, setShowMap] = useState(false);
+
+  const openMapHandler = () => setShowMap(true);
+  const closeMapHandler = () => setShowMap(false);
+
   return (
-    <li className="place-item">
-      <Card className="place-item__content">
-        <div className="place-item__image">
-          <img src={props.image} alt={props.title}></img>
+    <>
+      <Modal
+        show={showMap}
+        onCancel={closeMapHandler}
+        header={props.address}
+        contentClass="place-item__modal-content"
+        footerClass="place-item__modal-actions"
+        footer={<Button onClick={closeMapHandler}>Close</Button>}
+      >
+        <div className="map-container" style={{ padding: "5px" }}>
+          <iframe
+            title="map"
+            width="100%"
+            height="100%"
+            style={{ border: "0" }}
+            src={
+              "https://maps.google.com/maps?q=" +
+              props.coordinates.lat.toString() +
+              "," +
+              props.coordinates.lng.toString() +
+              "&t=&z=15&ie=UTF8&iwloc=&output=embed"
+            }
+          ></iframe>
         </div>
-        <div className="place-item__info">
-          <h2>{props.title}</h2>
-          <h3>{props.address}</h3>
-          <p>{props.description}</p>
-        </div>
-        <div className="place-item__actions">
-          <Button inverse>View On Map</Button>
-          <Button to={`/places/${props.id}`}>Edit</Button>
-          <Button danger>Delete</Button>
-        </div>
-      </Card>
-    </li>
+      </Modal>
+      <li className="place-item">
+        <Card className="place-item__content">
+          <div className="place-item__image">
+            <img src={props.image} alt={props.title}></img>
+          </div>
+          <div className="place-item__info">
+            <h2>{props.title}</h2>
+            <h3>{props.address}</h3>
+            <p>{props.description}</p>
+          </div>
+          <div className="place-item__actions">
+            <Button inverse onClick={openMapHandler}>
+              View On Map
+            </Button>
+            <Button to={`/places/${props.id}`}>Edit</Button>
+            <Button danger>Delete</Button>
+          </div>
+        </Card>
+      </li>
+    </>
   );
 };
 
