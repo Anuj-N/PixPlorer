bafe529d2c1b452a6082ad7e972d19c7bb32841b
ANUJ NETHWEWALA <anujnethwewala@ANUJs-MacBook-Pro-2.local>
2024-11-02 13:10:04 +0530

    Setup Login page frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import Users from "./users/pages/Users";
 import NewPlace from "./places/pages/NewPlace";
 import UserPlaces from "./places/pages/UserPlaces";
 import UpdatePlace from "./places/pages/UpdatePlace";
+import Auth from "./users/pages/Auth";
 import MainNavigation from "./shared/components/Navigation/MainNavigation";
 
 function App() {
@@ -26,6 +27,7 @@ function App() {
             ></Route>
             <Route path="/places/new" exact element={<NewPlace />}></Route>
             <Route path="/places/:placeId" element={<UpdatePlace />}></Route>
+            <Route path="/auth" element={<Auth />}></Route>
             <Route path="*" element={<Navigate to="/" />}></Route>
           </Routes>
         </main>
