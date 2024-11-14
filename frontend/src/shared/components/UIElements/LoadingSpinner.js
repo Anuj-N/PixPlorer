1f9a9c4d731726e7bc6d5327a9c9496b0b275b6f
ANUJ NETHWEWALA <anujnethwewala@ANUJs-MacBook-Pro-2.local>
2024-11-14 09:49:11 +0530

    Modify Loading Spinner message

--- frontend/src/shared/components/UIElements/LoadingSpinner.js
+++ frontend/src/shared/components/UIElements/LoadingSpinner.js
@@ -7,7 +7,7 @@ const LoadingSpinner = (props) => {
   return (
     <div className={`${props.asOverlay && "loading-spinner__overlay"}`}>
       <Card>
-        <h4>Please wait, initial loading may take a minute</h4>
+        <h4>Please wait, loading may take upto a minute</h4>
         <div className="lds-dual-ring"></div>
       </Card>
     </div>
